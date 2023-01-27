497b6cdf59f1bb5122b1a942965e454ce2d5555b
Peter Heuer <pvheuer@gmail.com>
2023-01-27 15:41:43 -0500

    Create test_export_import.py

---
+++ prepper/tests/test_export_import.py
@@ -0,0 +1,149 @@
+import pytest
+import os
+import numpy as np
+import h5py
+import inspect
+
+from prepper.exportable import ExportableClassMixin
+from prepper.io_handlers import saveable_class
+
+
+@saveable_class("0.0.1")
+class ClassWithinAClass(ExportableClassMixin):
+    _exportable_attributes = ['test_string']
+    def __init__(self):
+        self.test_string = 'test string ClassWithinAClass'
+        
+        
+class NotASaveableClass():
+    def __init__(self):
+        self.test_int = 1
+
+# Varaible types to try and save
+saveable_variables = [
+     (None), # None
+     (1), # int
+     (1.1), # float
+     ("testing testing"), # string
+     (np.float32(1.1)), # np.float32
+     (np.ones(10, dtype=np.float32)), # ndarray float32
+     (np.random.random((5, 3, 2))), # ndarray 3D
+     ({'key1':1, 2:'two', 3.3:3}), # dict with string, int, float
+     ([1, 2, 3]), # list (all the same datatype)
+     ([1, 'two', 3.3]), # List (all different datatypes)
+     (ClassWithinAClass()), # Saveable class
+     
+]
+
+# Object, error, msg
+unsaveable_variables = [
+     (NotASaveableClass(), ValueError,
+      "that does not support being saved to an HDF5 file!"), # Not a saveable class
+     
+]
+
+
+@pytest.mark.parametrize("var", saveable_variables)
+def test_export_import_mixin(var, tmp_path):
+    """
+    Test that you can export and import an object with every supported type
+    """
+    
+    @saveable_class("0.0.1")
+    class ThisClassHasAVariable(ExportableClassMixin):
+        _exportable_attributes = ['test_var']
+        def __init__(self):
+            self.test_var = var
+
+    path = os.path.join(tmp_path, 'tmp.hdf5')
+    
+    obj1 = ThisClassHasAVariable()
+    obj1.to_hdf5(path)
+    
+    obj2 = ThisClassHasAVariable.from_hdf5(path)
+    
+    # Assert that ExportableClassMixin thinks the two objects are equal
+    assert obj1 == obj2
+    
+    # Check that the imported object actually has the same type as the
+    # original
+    # 
+    # TODO: Confirm that we actually want to be saving python int and float
+    # as numpy types
+    if type(var)==int:
+        assert type(obj2.test_var) == np.int32
+    elif type(var)==float:
+        assert type(obj2.test_var) == np.float64  
+    else:
+        assert type(obj2.test_var) == type(var)
+    
+
+
+
+@pytest.mark.parametrize("var", saveable_variables)
+def test_export_hdf5_format(var, tmp_path):
+    """
+    Test that you can export and import an object with every supported type
+    """
+    
+    @saveable_class("0.0.1")
+    class ThisClassHasAVariable(ExportableClassMixin):
+        _exportable_attributes = ['test_var']
+        def __init__(self):
+            self.test_var = var
+
+    path = os.path.join(tmp_path, 'tmp.hdf5')
+    
+    obj1 = ThisClassHasAVariable()
+    obj1.to_hdf5(path)
+    
+    with h5py.File(path, 'r') as f:
+        
+        req_class_attrs = ['module', 'class', 'timestamp', 'version', 'code',
+                     'type', 'api_version']
+        
+        req_dataset_attrs = ['timestamp', 'type']
+        
+        # Test that the base level attributes are all there
+        for attr in req_class_attrs:
+            assert attr in f.attrs.keys()
+        
+        
+        # Test that the exportable attribute was exported
+        assert 'test_var' in f.keys()
+        
+
+        # Test that the exportable attribute has all the required attributes
+        if inspect.isclass(var):
+            req_attrs = req_class_attrs
+        elif var is None:
+            req_attrs = ['type']
+        else:
+            req_attrs = req_dataset_attrs
+        for attr in req_attrs:
+            assert attr in f['test_var'].attrs.keys()
+
+
+
+
+
+@pytest.mark.parametrize("var,error,msg", unsaveable_variables)
+def test_error_during_export(var, error, msg, tmp_path):
+    """
+    Verify that expected exceptions are raised
+    """
+    path = os.path.join(tmp_path, 'tmp.hdf5')
+    
+    @saveable_class("0.0.1")
+    class ThisClassHasABadVariable(ExportableClassMixin):
+        _exportable_attributes = ['test_var']
+        def __init__(self):
+            self.test_var = var
+    
+    obj = ThisClassHasABadVariable()
+
+    with pytest.raises(error, match=msg):
+        obj.to_hdf5(path)
+
+
+
